1c0c84c8ced778b6ff21f71e0ea741b45803edd4
Piotr Skubis <58171983+psqbs@users.noreply.github.com>
2024-09-24 09:06:05 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel/serverless';
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
+  build: {
+    format: 'file'
+  },
 });
